c65c7926c92e58317240a6d86f766f9a6435828d
a.pushkin <a.v.pushkin@gmail.com>
2022-10-28 15:59:19 +0300

    update

--- application/generator.py
+++ application/generator.py
@@ -1,6 +1,6 @@
 from random_username.generate import generate_username
 
 def generate():
-    quantity = input("Введите количество необходимых имён пользователей:")
+    quantity = input("Введите количество необходимых имён пользователей: ")
     print(generate_username(int(quantity)))
 
