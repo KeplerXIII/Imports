bebf04648cc5564cfd0b734caacc3ef0f37733d3
a.pushkin <a.v.pushkin@gmail.com>
2022-10-28 15:41:31 +0300

    update

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from datetime import date
+
+from application.salary import calculate_salary
+from application.db.people import get_employees
+from application.generator import generate
+
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print(f'Текущая дата - {date.today()}')
+    generate()
+    calculate_salary()
+    get_employees()
